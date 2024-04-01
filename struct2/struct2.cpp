d0ec3a2dd3b12d5ace2f9d36b4d42cb96726293d
voltarou <bagusadnan04@gmail.com>
2024-04-01 15:26:40 +0800

    int main pt1

--- struct2/struct2.cpp
+++ struct2/struct2.cpp
@@ -5,9 +5,18 @@ struct DetailAlamat {
 	string desa;
 	string kota;
 };
+
 struct Mahasiswa {
 	string nim;
 	string nama;
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+}
