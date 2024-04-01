394695b06ed2979b11e993e0371541d31d86aa11
voltarou <bagusadnan04@gmail.com>
2024-04-01 16:36:48 +0800

    struct3 rework

--- struct4/struct4.cpp
+++ struct4/struct4.cpp
@@ -12,3 +12,14 @@ struct Mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor mahasiswa : ";
+		getline: (cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		
+	}
+}
