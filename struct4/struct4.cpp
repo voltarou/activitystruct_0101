63648d56279d0a0d156b9e35762074b55bf46be8
voltarou <bagusadnan04@gmail.com>
2024-04-01 16:56:41 +0800

    for3

--- struct4/struct4.cpp
+++ struct4/struct4.cpp
@@ -21,5 +21,16 @@ int main() {
 		getline: (cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
 		
+		cout << "Alamat Mahasiswa : " << endl;
+		cout << "\t Nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama Kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur Mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
 	}
+
+	
 }
